79e4b143fba50695d5d81b2f721098f865680f1b
nikithapk <nikithapoolanda@gmail.com>
2020-04-28 18:55:30 +0530

    increment/decrement added

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -12,9 +12,43 @@ export class CartComponent implements OnInit {
   ngOnInit(): void {
   }
   cartproducts = [
-    { name: "Coke", quantity  : 1 , price: "35.00" , imgurl : "assets/img/coke.jpg" },
-    { name: "Sprite", quantity  : 2 , price: "45.00" , imgurl : "assets/img/sprite.jpg" },
-    { name: "Pepsi", quantity  :1 , price: "25.00" , imgurl : "assets/img/pepsi.jpg" },
-    { name: "Maaza", quantity  : 4 , price: "60.00" , imgurl : "assets/img/maaza.jpg" }
+    { name: "Coke", quantity  : 1 , price: "35.00" , imgurl : "assets/img/coke.jpg" ,message:''},
+    { name: "Sprite", quantity  : 2 , price: "45.00" , imgurl : "assets/img/sprite.jpg" ,message:''},
+    { name: "Pepsi", quantity  :1 , price: "25.00" , imgurl : "assets/img/pepsi.jpg" ,message:''},
+    { name: "Maaza", quantity  : 4 , price: "60.00" , imgurl : "assets/img/maaza.jpg" ,message:''}
   ];
+
+
+  //Increment and decrement
+// message: string;
+
+increment(units:number,index:number) 
+{
+  if(units < 15){
+    this.cartproducts[index].quantity += 1;  
+    this.cartproducts[index].message = ' ';   
   }
+  else
+  {
+    if(units >=15)
+    {
+      this.cartproducts[index].message = 'Maximum reached!';
+    }
+ } 
+}
+
+decrement(units:number,index:number) 
+{
+  if(units > 0){
+    this.cartproducts[index].quantity -= 1;  
+    this.cartproducts[index].message = ' ';  
+  }
+  else if(units==0)
+    {
+      this.cartproducts[index].message = 'Minimum reached!';
+    }
+       
+  }
+
+
+}
